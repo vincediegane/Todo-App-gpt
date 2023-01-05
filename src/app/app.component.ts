e694ce0914e0d08e132b9138d20e0d74e0c6a249
vincent diegane faye <vincediegane@gmail.com>
2023-01-05 17:25:33 +0100

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-app';
   todos = [
     { title: 'todo 1', done: true },
     { title: 'todo 2', done: false },
     { title: 'todo 3', done: true },
   ]
+  newTodo: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addTodo() {}
+
+  removeTodo(todo: any) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
